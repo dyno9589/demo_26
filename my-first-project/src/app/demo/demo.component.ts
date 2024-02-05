a8c439a0801ac7ded3ef53f8f5e49c8518f9d27d
Dinesh Prajapati <56971781+dyno9589@users.noreply.github.com>
2024-02-05 10:50:28 +0000

    uploading angular

---
+++ my-first-project/src/app/demo/demo.component.ts
@@ -0,0 +1,13 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './demo.component.html',
+  styleUrl: './demo.component.css'
+})
+export class DemoComponent {
+  arr=['a','e','i'];
+  
+}
